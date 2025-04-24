c15f610f70f8d792e793c464cf1d63255a3d1681
Jason Ross <jason@Jasons-MacBook-Pro.local>
2025-04-23 23:29:35 -0500

    Conditional Rendering for SSR/SSG

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // If using React Router
+import { Link } from 'react-router-dom';
 
 export default function FourOhFourPage() {
+  const isBrowser = typeof window !== 'undefined';
+
   return (
     <div className="item1">
       <h1>Oh No - HTTP 404 🙁</h1>
       <p>Hey there....this page does not exist! If you feel this is an error, open an issue on my <a href="https://github.com/jarossnd/jason-ross-dev-v2/issues">Git Hub Site</a></p>
-      <Link to="/" className="btn">Go Back to Home</Link>
+      {isBrowser ? (
+        <Link to="/" className="btn">Go Back to Home</Link>
+      ) : (
+        <a href="/" className="btn">Go Back to Home</a>
+      )}
     </div>
   );
 }
